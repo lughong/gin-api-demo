cd6f836c635ff16ed3f72efcc77917551ff84bf4
Your Name <you@example.com>
2019-12-08 18:29:57 +0800

    change handler/v1 autoload position

--- router/router.go
+++ router/router.go
@@ -1,10 +1,10 @@
 package router
 
 import (
-	v1 "github.com/lughong/gin-api-demo/handler/v1"
 	"net/http"
 	"time"
 
+	v1 "github.com/lughong/gin-api-demo/handler/v1"
 	"github.com/lughong/gin-api-demo/router/middleware"
 
 	"github.com/gin-contrib/cors"
